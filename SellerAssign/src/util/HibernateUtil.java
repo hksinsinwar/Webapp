d2959dd7531179f0b41a8b13b1649c486508d09b
Harendra Kumar <hiiibdh@gmail.com>
2014-05-23 12:02:42 +0530

    Update HibernateUtil.java

--- SellerAssign/src/util/HibernateUtil.java
+++ SellerAssign/src/util/HibernateUtil.java
@@ -4,12 +4,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	private static SessionFactory sessionFactory = null;
+	private static volatile SessionFactory sessionFactory = null;
 
 	public static SessionFactory getSessionFactory() {
-		if (sessionFactory == null)
-			sessionFactory = new Configuration().configure(
+		SessionFactory inst = sessionFactory;
+		if (inst == null){
+		synchronized(this){	
+			inst=sessionFactory;
+			if(inst == null){
+			inst=sessionFactory = new Configuration().configure(
 					"config/hibernate.cfg.xml").buildSessionFactory();
+			}
+		}
+		}
 		return sessionFactory;
 	}
 }
